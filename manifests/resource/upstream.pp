ce7c6084d17793859e212a3da0b1227e18bc53c9
Mingxiang.Zhang <ming.zhang@gree.net>
2013-04-08 19:48:24 +0900

    add check_path to nginx::resource::upstream

--- manifests/resource/upstream.pp
+++ manifests/resource/upstream.pp
@@ -4,7 +4,10 @@
 #
 # Parameters:
 #   [*ensure*]      - Enables or disables the specified location (present|absent)
-#   [*members*]     - Array of member URIs for NGINX to connect to. Must follow valid NGINX syntax.
+#   [*members*]     - Array of member URIs for NGINX to connect to. Must follow valid NGINX 
+syntax.
+#   [*check*]       - check configuration of heath-check of upstream
+#   [*check_path*]  - relative path of heath-check url
 #
 # Actions:
 #
@@ -22,7 +25,8 @@
 define nginx::resource::upstream (
   $ensure = 'present',
   $members,
-  $check = false
+  $check = false,
+  $check_path = false
 ) {
   File {
     owner => 'root',
